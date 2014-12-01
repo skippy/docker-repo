466bc7214f78ccf5f86445860f24b33696a411c5
Adam Greene <adam.greene@gmail.com>
2014-11-30 20:21:34 -0800

    tweaks to fleet configs

--- elasticsearch/fleet/start.sh
+++ elasticsearch/fleet/start.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-set -eo pipefail
+# set -eo pipefail
 
-
+echo "stopping elasticsearch_logging"
 fleetctl stop \
 	elasticsearch_logging@{1..2}.service \
 	elasticsearch_logging-kibana.service
-
 # FIXME: sometimes it takes awhile for systemd to stop things... 
 # if we go right to destroy this can leave services running...
-
+echo "destroying elasticsearch_logging"
 fleetctl destroy \
 	elasticsearch_logging@.service \
 	elasticsearch_logging-discovery@.service \
@@ -20,18 +19,22 @@ fleetctl destroy \
 
 # FIXME: fleetctl can have a consistancy problem; if we submit and load too soon
 #        after a destroy call, it may load up an older version of the service...
-sleep(5)
-
+echo "sleeping and then submitting elasticsearch_logging"
+SCRIPT=`readlink -f "$0"`
+SCRIPTPATH=`dirname "$SCRIPT"`
+sleep 5
 fleetctl submit \
-	share/elasticsearch/fleet/elasticsearch_logging@.service \
-	share/elasticsearch/fleet/elasticsearch_logging-discovery@.service \
-	share/elasticsearch/fleet/elasticsearch_logging-startup_lease@.service \
-	share/elasticsearch/fleet/elasticsearch_logging-kibana.service
+	"$SCRIPTPATH/elasticsearch_logging@.service" \
+	"$SCRIPTPATH/elasticsearch_logging-discovery@.service" \
+	"$SCRIPTPATH/elasticsearch_logging-startup_lease@.service" \
+	"$SCRIPTPATH/elasticsearch_logging-kibana.service"
 
+echo "loading elasticsearch_logging"
 fleetctl load \
 	elasticsearch_logging@{1..2}.service \
 	elasticsearch_logging-discovery@{1..2}.service \
 	elasticsearch_logging-startup_lease@{1..2}.service \
 	elasticsearch_logging-kibana.service
 
+echo "starting elasticsearch_logging"
 fleetctl start elasticsearch_logging@{1..2}.service
